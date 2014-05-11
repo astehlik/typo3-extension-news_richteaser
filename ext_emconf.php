b816105098e35ed8760223ad19d0785e0fede56d
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-05-11 14:23:35 +0200

    [TASK] Raised TYPO3 version requirement
    For the new TCA override mechanism we require at least TYPO3 6.2.1.

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.1-6.2.99',
 			'news' => '',
 		),
 		'conflicts' => array(
